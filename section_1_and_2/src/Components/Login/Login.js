03bddaa78a8982a3b7453193f7007668a3f842dc
Bhartendra Singh Chauhan <65101955+bhartendrachauhan@users.noreply.github.com>
2023-05-28 02:46:27 +0530

    Implemented Login UI

--- section_1_and_2/src/Components/Login/Login.js
+++ section_1_and_2/src/Components/Login/Login.js
@@ -1,16 +1,42 @@
-import React from 'react'
-import './Login.css'
+import React from "react";
+import "./Login.css";
 
 const Login = () => {
   return (
     <>
-    <div className='login-outer-container'>
-      <div className='login-container'>
-      <i className="fa-solid fa-lock fa-3x"></i>
-      </div>
-    </div>
-    </>
-  )
-}
+      <div className="login-outer-container">
+        <div className="login-container">
+          <div className="login-message-container">
 
-export default Login
+          </div>
+          <i className="fa-solid fa-lock fa-3x"></i>
+          <h2 style={{ marginBottom: "40px" }}>Login</h2>
+          <div className="login-each-container">
+            <i className="fa-solid fa-envelope fa-lg"></i>
+            <input
+              type="text"
+              className="login-each-input"
+              placeholder="Email"
+            />
+          </div>
+          <div className="helper-text"></div>
+          <div className="login-each-container">
+            <i className="fa-solid fa-key fa-lg"></i>
+            <input
+              type="password"
+              className="login-each-input"
+              placeholder="Password"
+            />
+          </div>
+          <div className="helper-text"></div>
+          <div className="login-button-container">
+            Login
+            <button className="login-button"></button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
